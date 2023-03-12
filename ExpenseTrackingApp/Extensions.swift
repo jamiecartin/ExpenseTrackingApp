a58850ef92a88cf14ee0e6a449f158973205e4b8
jamiecartin <jamie.cartin@icloud.com>
2023-03-12 17:04:32 -0400

    set up extension for color and assets

---
+++ ExpenseTrackingApp/Extensions.swift
@@ -0,0 +1,16 @@
+//
+//  Extensions.swift
+//  ExpenseTrackingApp
+//
+//  Created by Jamie Cartin on 3/12/23.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    static let background = Color("Background")
+    static let icon = Color("Icon")
+    static let text = Color("Text")
+    static let systemBackground = Color(uiColor: .systemBackground)
+}
