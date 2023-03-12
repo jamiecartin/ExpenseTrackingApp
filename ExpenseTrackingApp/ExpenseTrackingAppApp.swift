e7659e6a357fb0701dbbef56043b6a7fbef0f3af
jamiecartin <jamie.cartin@icloud.com>
2023-03-11 22:57:22 -0500

    Initial Commit

---
+++ ExpenseTrackingApp/ExpenseTrackingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseTrackingAppApp.swift
+//  ExpenseTrackingApp
+//
+//  Created by Jamie Cartin on 3/11/23.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseTrackingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
