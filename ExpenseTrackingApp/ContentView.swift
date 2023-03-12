975533695a6621eca04d53dbfc62632876769704
jamiecartin <jamie.cartin@icloud.com>
2023-03-12 17:23:14 -0400

    set up default colors and added notification bell alert

--- ExpenseTrackingApp/ContentView.swift
+++ ExpenseTrackingApp/ContentView.swift
@@ -9,14 +9,21 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            ScrollView {
+                
+            }
+            .background(Color.background)
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem {
+                    Image(systemName: "bell.badge")
+                        .symbolRenderingMode(.palette)
+                        .foregroundStyle(Color.icon, .primary)
+                }
+            }
         }
-        .padding()
-        
+        .navigationViewStyle(.stack)
     }
 }
 
