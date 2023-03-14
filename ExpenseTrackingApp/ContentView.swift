b63c9c31f995277743d06299d545faba6c2232f2
jamiecartin <jamie.cartin@icloud.com>
2023-03-14 00:06:53 -0400

    Add charts, fix chart view, display transaction data in currency

--- ExpenseTrackingApp/ContentView.swift
+++ ExpenseTrackingApp/ContentView.swift
@@ -6,16 +6,39 @@
 //
 
 import SwiftUI
+import SwiftUICharts
 
 struct ContentView: View {
+    @EnvironmentObject var transactionListVM: TransactionListViewModel
+    //var demoData: [Double] = [8, 2, 4, 6, 12, 9, 2]
+    
     var body: some View {
         NavigationView {
             ScrollView {
                 VStack(alignment: .leading, spacing: 24) {
+                    //MARK: Title
                     Text("Overview")
                         .font(.title2)
                         .bold()
                     
+                    // MARK: Chart
+                    let data = transactionListVM.accumulateTransactions()
+                    
+                    if !data.isEmpty {
+                        let totalExpenses = data.last?.1 ?? 0
+                        CardView(showShadow: false) {
+                            VStack(alignment: .leading) {
+                                ChartLabel(totalExpenses.formatted(.currency(code: "USD")), type: .title, format: "$%.02f")
+                                LineChart()
+                            }
+                        }
+                        .data(data)
+                        .chartStyle(ChartStyle(backgroundColor: Color.systemBackground, foregroundColor: ColorGradient(Color.icon.opacity(0.4), Color.icon)))
+                        .frame(height: 300)
+                    }
+                   
+                    
+                    //MARK: Transaction List
                     RecentTransactionList()
                 }
                 .padding()
--- ExpenseTrackingApp/TransactionListViewModel.swift
+++ ExpenseTrackingApp/TransactionListViewModel.swift
@@ -10,6 +10,7 @@ import Combine
 import Collections
 
 typealias TransactionGroup = OrderedDictionary<String, [Transaction]>
+typealias TransactionPrefixSum = [(String, Double)]
 
 final class TransactionListViewModel: ObservableObject {
     @Published var transactions: [Transaction] = []
@@ -56,5 +57,28 @@ final class TransactionListViewModel: ObservableObject {
         
         let groupTransactions = TransactionGroup(grouping: transactions) { $0.month }
         return groupTransactions
+    }
+    
+    func accumulateTransactions() -> TransactionPrefixSum {
+        print("accumulateTransactions")
+        guard !transactions.isEmpty else { return [] }
+        
+        let today = "2/17/2022".dateParsed() // should be Date() in production/ with live data
+        let dateInterval = Calendar.current.dateInterval(of: .month, for: today)!
+        print("dateInterval")
+        
+        var sum: Double = .zero
+        var cumulativeSum = TransactionPrefixSum()
+        
+        for date in stride(from: dateInterval.start, through: today, by: 60 * 60 * 24) {
+            let dailyExpenses = transactions.filter { $0.dateParsed == date && $0.isExpense }
+            let dailyTotal = dailyExpenses.reduce(0) { $0 - $1.signedAmount }
+            
+            sum += dailyTotal
+            sum = sum.roundedTo2Digits()
+            cumulativeSum.append((date.formatted(), sum))
+            print(date.formatted(), "dailyTotal:", dailyTotal, "sum:", sum)
+        }
+        return cumulativeSum
     }
 }
